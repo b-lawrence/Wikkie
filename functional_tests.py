1a1e4f738d1cbe17f852600aa966dc01374aa3dd
Lawrence Olaiya <lawrence.olaiya@andela.com>
2016-04-17 17:57:20 +0100

    switch to using unittest, look to 'To-Do in browser title'

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,26 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+
+    def tearDown(self):
+        self.browser.quit()
+
+
+    def test_creating_a_new_list(self):
+
+        # Edith has heard about a cool new to-do app. She goes to
+        # check it out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists.
+        self.assertIn('To-Do', self.browser.title)
+
+
+# Run tests
+if __name__ == "__main__":
+    unittest.main()
