906b929c28d6d1793ef5b233d0d145e400cdf59b
Lawrence Olaiya <lawrence.olaiya@andela.com>
2016-04-28 12:08:30 +0100

    Update views to accomodate page edit and to load a page with fill markdown Add templates for page edit and display Add current dependency list to requirements.txt

--- wiki/views.py
+++ wiki/views.py
@@ -1,44 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from markdown import markdown
+import bleach
 from .forms import PageForm
 from .models import Page
-
-
-def page(request, slug):
-    page = None
-
-    try:
-        page = Page.objects.get(slug=slug)
-    except Page.DoesNotExist:
-        pass
-
-    return render(request, "page.html", context={
-        "page": page,
-        "slug": slug})
 
 
 def home_page(request):
     return render(request, "home.html", context={"title": "Wikkie"})
 
-def page_edit(request, slug):
-    page = None
-
-    if request.method == 'GET':
-        page_form = PageForm()
-
-    elif request.method == 'POST':
-        page_form = PageForm(request.POST)
-        saved_page = page_form.save()
-        saved_page.slug = slug
-        saved_page.save()
-
-    return render(request, "edit_page.html", context={
-        "page": page,
-        "slug": slug,
-        "page_form": page_form})
-
+def page(request, slug):
     try:
         page = Page.objects.get(slug=slug)
     except Page.DoesNotExist:
-        pass
+        page = None
+
+    allowed_tags = bleach.ALLOWED_TAGS + ['p', 'h1', 'h2', 'h3']
+    page_content = bleach.clean(markdown(page.content), tags=allowed_tags) if page else None
+    page_title = page.titie if page else slug
+
+    return render(request, "page.html", context={
+        "page": page,
+        "slug": slug,
+        "title": page_title,
+        "page_content": page_content})
+
+def page_edit(request, slug):
+    try:
+        page = Page.objects.get(slug=slug)
+    except Page.DoesNotExist:
+        page = Page(slug=slug)
+
+    if request.method == 'GET':
+        page_form = PageForm(instance=page)
+    elif request.method == 'POST':
+        page_form = PageForm(request.POST, instance=page)
+        if page_form.is_valid():
+            page_form.save()
+            return redirect("page", slug)
+
+    return render(request, "edit_page.html", context={
+        "slug": slug,
+        "title": page.title or page.slug,
+        "page_form": page_form})
