972d79485d4f0e99bceb7d8d17b886909e91446a
Lawrence Olaiya <lawrence.olaiya@andela.com>
2016-05-02 20:09:08 +0100

    Add methods to represent slug as a string and to get absolute urls for slugs links.

--- wiki/models.py
+++ wiki/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.core.urlresolvers import reverse
+
 
 # Create your models here.
 class Page(models.Model):
 
-    def __str__(self):
-        return "<Page: slug={}".format(self.slug)
-
     slug = models.SlugField(max_length=50, unique=True)
     title = models.CharField(max_length=50)
     content = models.TextField()
+
+    def __str__(self):
+        return "slug={}".format(self.slug)
+
+    def get_absolute_url(self):
+        return reverse("page", args=[self.slug])
