7b1b1f2abccb764b41e3768503f34a0e3d431e51
Lawrence Olaiya <lawrence.olaiya@andela.com>
2016-05-02 16:38:21 +0100

    Update form attributes Remove form labels

--- wiki/forms.py
+++ wiki/forms.py
@@ -8,14 +8,31 @@ class PageForm(forms.ModelForm):
         model = Page
         fields = ['title', 'content']
         widgets = {
-                  'title': forms.TextInput(attrs={'class': 'form-control'}),
-                  'content': forms.Textarea(attrs={
-                                            'cols': 80,
-                                            'rows': 20,
-                                            'class': 'form-control'})}
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Page title'}),
+            'content': forms.Textarea(attrs={
+                'cols': 80,
+                'rows': 20,
+                'class': 'form-control',
+                'placeholder': 'Content goes here ...'})
+            }
+
 
 class NewPageForm(PageForm):
 
     class Meta(PageForm.Meta):
         fields = ['slug', 'title', 'content']
-        
+        widgets = {
+            'slug': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Slug'}),
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Page title'}),
+            'content': forms.Textarea(attrs={
+                'cols': 80,
+                'rows': 20,
+                'class': 'form-control',
+                'placeholder': 'Content goes here ...'})
+            }
