daaaab5a0e174e6c51683a915517b8d19bc813ce
Lawrence Olaiya <lawrence.olaiya@andela.com>
2016-05-02 04:11:05 +0100

    Add form attributes to PageForm.

--- wiki/forms.py
+++ wiki/forms.py
@@ -1,9 +1,15 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Page
 
 
-class PageForm(ModelForm):
+class PageForm(forms.ModelForm):
 
     class Meta:
         model = Page
         fields = ['title', 'content']
+        widgets = {
+                  'title': forms.TextInput(attrs={'class': 'form-control'}),
+                  'content': forms.Textarea(attrs={
+                                            'cols': 80,
+                                            'rows': 20,
+                                            'class': 'form-control'})}
