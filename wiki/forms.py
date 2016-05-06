319ccd7b0dbbd7db37c9d34d89f62e996c2e369b
Lawrence Olaiya <lawrence.olaiya@andela.com>
2016-05-06 18:32:34 +0100

    Add custom login form Modify PageForm and NewPageForm

--- wiki/forms.py
+++ wiki/forms.py
@@ -1,12 +1,13 @@
 from django import forms
 from django.contrib.auth.models import User
-from .models import Page
+from django.contrib.auth.forms import AuthenticationForm
+from .models import PageVersion
 
 
 class PageForm(forms.ModelForm):
 
     class Meta:
-        model = Page
+        model = PageVersion
         fields = ['title', 'content']
         widgets = {
             'title': forms.TextInput(attrs={
@@ -21,22 +22,14 @@ class PageForm(forms.ModelForm):
 
 
 class NewPageForm(PageForm):
+    slug = forms.SlugField(max_length=50,
+                           widget=forms.TextInput(attrs={
+                                'class': 'form-control',
+                                'placeholder': 'Slug'})
+                           )
 
     class Meta(PageForm.Meta):
         fields = ['slug', 'title', 'content']
-        widgets = {
-            'slug': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Slug'}),
-            'title': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Page title'}),
-            'content': forms.Textarea(attrs={
-                'cols': 80,
-                'rows': 20,
-                'class': 'form-control',
-                'placeholder': 'Content goes here ...'})
-            }
 
 
 class SignupForm(forms.ModelForm):
@@ -45,5 +38,27 @@ class SignupForm(forms.ModelForm):
         model = User
         fields = ['username', 'email', 'password']
         widgets = {
-            'password': forms.PasswordInput()
+            'username': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Desired username'}),
+            'email': forms.TextInput(attrs={
+                'type': 'email',
+                'class': 'form-control',
+                'placeholder': 'Your email'}),
+            'password': forms.PasswordInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Password'})
         }
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(max_length=50,
+                               widget=forms.TextInput(attrs={
+                                    'class': 'form-control',
+                                    'placeholder': 'username'}
+                               ))
+    password = forms.CharField(max_length=30,
+                               widget=forms.PasswordInput(attrs={
+                                    'class': 'form-control',
+                                    'placeholder': 'password'}
+                               ))
