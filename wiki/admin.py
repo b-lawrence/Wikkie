2435c581ce9877dfcd40857761d7e5b30615688e
Lawrence Olaiya <lawrence.olaiya@andela.com>
2016-05-06 18:37:13 +0100

    Register Page model with django admin

--- wiki/admin.py
+++ wiki/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Page
 
 # Register your models here.
+
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    pass
+    # list_display = ('title', 'author', 'updated_at')
