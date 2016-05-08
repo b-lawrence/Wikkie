aa99f9553fc165c3b9c509b686c390f2037e713b
Lawrence Olaiya <lawrence.olaiya@andela.com>
2016-05-08 22:14:16 +0100

    Use get_user_model to retrive the User model Refactor new_page view to use page versioning

--- wiki/views.py
+++ wiki/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from markdown import markdown
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.contrib.auth import login, authenticate
+from markdown import markdown
 import bleach
 
 from . import forms
@@ -13,6 +13,7 @@ from .models import Page
 
 def home_page(request):
     pages = Page.objects.all()
+    # TODO: Render page authors along with pages
     return render(request, "wiki/home.html", context={
         "title": "Wikkie",
         "pages": pages})
@@ -20,19 +21,23 @@ def home_page(request):
 
 def page(request, slug):
     try:
-        page = Page.objects.get(slug=slug)
+        page = Page.objects.get(slug=slug).best_version
     except Page.DoesNotExist:
         page = None
 
-    allowed_tags = bleach.ALLOWED_TAGS + ['p', 'h1', 'h2', 'h3']
-    page_content = bleach.clean(markdown(page.content), tags=allowed_tags) if page else None
-    page_title = page.title if page else slug
+    if page:
+        allowed_tags = bleach.ALLOWED_TAGS + ['p', 'h1', 'h2', 'h3']
+        processed_page_content = bleach.clean(
+            markdown(page.content),
+            tags=allowed_tags
+        )
+        page.content = processed_page_content
+        # TODO: Display author's name on the page decs page.
 
     return render(request, "wiki/page.html", context={
-        "page": page,
         "slug": slug,
-        "page_title": page_title,
-        "page_content": page_content})
+        "page": page
+    })
 
 
 @login_required
@@ -42,8 +47,14 @@ def new_page(request):
     elif request.method == 'POST':
         page_form = forms.NewPageForm(request.POST)
         if page_form.is_valid():
-            page_form.save()
-            return redirect("page", page_form.cleaned_data['slug'])
+            cleaned_form_data = page_form.cleaned_data
+            page_form_instance_on_save = page_form.save(commit=False)
+            page_form_instance_on_save.page = Page.objects.create(
+                slug=cleaned_form_data['slug']
+            )
+            page_form_instance_on_save.author = request.user
+            page_form_instance_on_save.save()
+            return redirect("page", cleaned_form_data['slug'])
 
     return render(request, "wiki/edit_page.html", context={
         "page_title": "Create a new wiki page.",
@@ -53,7 +64,7 @@ def new_page(request):
 @login_required
 def page_edit(request, slug):
     try:
-        page = Page.objects.get(slug=slug)
+        page = Page.objects.get(slug=slug).best_version
     except Page.DoesNotExist:
         page = Page(slug=slug)
 
@@ -66,7 +77,7 @@ def page_edit(request, slug):
             return redirect("page", slug)
 
     return render(request, "wiki/edit_page.html", context={
-        "page_title": page.title or page.slug,
+        "page": page,
         "page_form": page_form})
 
 
@@ -77,7 +88,8 @@ def register_user(request):
         signup_form = forms.SignupForm(request.POST)
         if signup_form.is_valid():
             form_data = signup_form.cleaned_data
-            User.objects.create_user(**form_data)
+            user_model = get_user_model()
+            user_model.objects.create_user(**form_data)
             user = authenticate(**form_data)
             login(request, user)
             return redirect("home")
